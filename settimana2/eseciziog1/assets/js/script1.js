5c368004ca6a8659b621aef89b2b00df14aeebf4
Davepuffo <123974851+Davepuffo@users.noreply.github.com>
2023-02-06 16:56:07 +0100

    esecizioWeek2g1

---
+++ settimana2/eseciziog1/assets/js/script1.js
@@ -0,0 +1,4 @@
+document.getElementById('corpo').innerHTML = '<h1>Iniziamo la nostra pratica</h1><p>output scritto da Javascript</p>';
+
+window.alert('Usa il pop up per dare dei messaggi')
+alert('Sto studiando JS')
---
+++ settimana2/eseciziog1/assets/js/script2.js
@@ -0,0 +1,11 @@
+var pantera = 'pantera';
+document.write(pantera + ' scritto da Javscript');
+
+var coniglio = 'coniglio';
+console.log(coniglio);
+
+var leone = 'leone';
+console.log(leone);
+
+var zebra = 'zebra';
+console.log(zebra);
