e7da4cdd559f10e35420dd94c692f32821314ef1
Davepuffo <123974851+Davepuffo@users.noreply.github.com>
2023-03-15 18:39:47 +0100

    eserciziow7g3

---
+++ settimana7/eserciziog3/script.js
@@ -0,0 +1,25 @@
+let listaLibri = document.getElementById('row1')
+
+fetch('https://striveschool-api.herokuapp.com/books')
+    .then((response) => {
+        console.log(response)
+        return response.json()
+    })
+    .then((data) => {
+        data.forEach((book) => {
+            const card = `
+            <div class="col-12 col-md-4 mb-4 h-25">
+                    <img src="${book.img}" class="card-img-top" alt="." />
+                    <div class="card-body">
+                        <h5>${book.title}</h5>
+                        <p class="card-text">${book.price}€</p>
+                        <button class="btn btn-primary">Scarta</button>
+                    </div>
+                </div>`;
+            listaLibri.innerHTML += card
+        });
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+
